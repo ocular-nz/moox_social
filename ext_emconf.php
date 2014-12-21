2e0c07495b8cfb864e0cc74eb04f480ad32c0c45
adrolli <ad@dcn.de>
2014-12-21 05:12:11 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,12 +27,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.9.0',
+	'version' => '0.9.5',
 	'constraints' => array(
 		'depends' => array(
-			'extbase' => '6.2.0-6.2.99',
-			'fluid' => '6.2.0-6.2.99',
 			'typo3' => '6.2.0-6.2.99',
+			'extbase' => '',
+			'fluid' => '',
 			'vhs' => '',
 		),
 		'conflicts' => array(
